e2ab4ff8e0027e36e80442def159fde7a3f9160b
wchan03 <123441599+wchan03@users.noreply.github.com>
2025-10-19 20:44:18 -0700

    uploaded to board
    todo:
    verify SPI signals
    connect website
    connect peripheral

---
+++ project/STM32L432KC_SPI.c
@@ -0,0 +1,80 @@
+// STM32L432KC_SPI.c
+// Wava Chan
+// wchan@g.hmc.edu
+// October 2025
+// SPI Library for the STM32L432KC Microcontroller
+
+#include "STM32L432KC_SPI.h"
+
+void initSPI(int br, int cpol, int cpha){
+    //turn on SPI clock
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN; 
+
+
+    // configure GPIO pins
+    gpioEnable(SCK);
+    gpioEnable(CIPO);
+    gpioEnable(COPI);
+    gpioEnable(CS);
+
+    pinMode(SCK, GPIO_ALT);
+    pinMode(CIPO, GPIO_ALT);
+    pinMode(COPI, GPIO_ALT);
+    pinMode(CS, GPIO_OUTPUT);
+
+    //set alternate function 5
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL5, 0b0101); //TODO: double check AFSELx numbers
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL6, 0b0101);
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL7, 0b0101);
+
+
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_BR, br);//set baud rate 
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPOL, cpol); //set clock polarity
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPHA, cpha); //set clock phase
+    SPI1->CR2 |= _VAL2FLD(SPI_CR2_DS, 0b0111);//configure data size tp 8 bit
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_LSBFIRST, 0); //enable MSB first
+    //SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSM, 1);//enable peripheral 
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_MSTR, 1);//enable controller 
+
+    SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRXTH, 1); //set FIFO to 8bits
+    SPI1->CR2 |= _VAL2FLD(SPI_CR2_SSOE, 1); //SS output is enabled
+    
+    //enable SPI
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_SPE, 0b1);
+
+}
+
+char spiSendReceive(char send){
+    while(!(SPI1->SR & SPI_SR_TXE)){}; // wait for empty transmit buffer
+    
+    // send data when it is written to a data register 
+    volatile char* address = (volatile char*)&SPI1->DR;
+    *address = send; 
+
+    while(!(SPI_SR_RXNE & SPI1->SR)){}; //wait for data to be recieved 
+
+    return *address; //return the receieve data
+}
+
+float decodeData(int msb, char lsb){
+    //get main data 
+    float temp = msb & 0b01111111; //turn it into a float
+    //get the sign bit 
+    int sign = msb & (1 << 7); //8th bit
+    if(!sign) { //when positive, add values
+        if(1 << 7 & lsb) temp = temp + 0.5;
+        if(1 << 6 & lsb) temp = temp + 0.25;
+        if(1 << 5 & lsb) temp = temp + 0.125;
+        if(1 << 4 & lsb) temp = temp + 0.0625;
+    }
+    else { //negative
+        temp = -(~(msb & 0b0111111) + 1);
+        if(1 << 7 & lsb) temp = temp - 0.5;
+        if(1 << 6 & lsb) temp = temp - 0.25;
+        if(1 << 5 & lsb) temp = temp - 0.125;
+        if(1 << 4 & lsb) temp = temp - 0.0625;
+
+    }
+
+    return temp;
+}
---
+++ project/STM32L432KC_SPI.h
@@ -0,0 +1,40 @@
+// STM32L432KC_SPI.h
+// Wava Chan
+// wchan@g.hmc.edu
+// October 2025
+// Header file for SPI functions
+
+#ifndef STM32L4_SPI_H
+#define STM32L4_SPI_H
+
+#include <stdint.h>
+#include <stm32l432xx.h>
+#include "STM32L432KC_GPIO.h"
+
+#define SCK PA5 //TODO: reassign these pins?
+#define CIPO PA6
+#define COPI PA7
+#define CS 4
+
+#define PORT GPIOA; 
+
+///////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+///////////////////////////////////////////////////////////////////////////////
+
+/* Enables the SPI peripheral and intializes its clock speed (baud rate), polarity, and phase.
+ *    -- br: (0b000 - 0b111). The SPI clk will be the master clock / 2^(BR+1).
+ *    -- cpol: clock polarity (0: inactive state is logical 0, 1: inactive state is logical 1).
+ *    -- cpha: clock phase (0: data captured on leading edge of clk and changed on next edge, 
+ *          1: data changed on leading edge of clk and captured on next edge)
+ * Refer to the datasheet for more low-level details. */ 
+void initSPI(int br, int cpol, int cpha);
+
+/* Transmits a character (1 byte) over SPI and returns the received character.
+ *    -- send: the character to send over SPI
+ *    -- return: the character received over SPI */
+char spiSendReceive(char send);
+
+float decodeData(int msb, char lsb); 
+
+#endif
