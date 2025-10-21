5bb9dfeadebad9dc199a931965c2a28651d74e06
wchan03 <123441599+wchan03@users.noreply.github.com>
2025-10-20 22:03:14 -0700

    file cleanup

--- project/STM32L432KC_SPI.c
+++ project/STM32L432KC_SPI.c
@@ -10,6 +10,7 @@ void initSPI(int br, int cpol, int cpha){
     //turn on SPI clock
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN; 
 
+
     // configure GPIO pins
     gpioEnable(SCK);
     gpioEnable(CIPO);
@@ -22,21 +23,25 @@ void initSPI(int br, int cpol, int cpha){
     pinMode(CS, GPIO_OUTPUT);
 
     //set alternate function 5
-    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL5, 0b0101); //PA5 AF5 = SPI1_SCK
-    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL6, 0b0101); //PA6 AF5 = SPI1_MISO
-    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL7, 0b0101); //PA7 AF5 = SPI1_MOSI
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL5, 0b0101); 
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL6, 0b0101);
+    GPIOA->AFR[0] |= _VAL2FLD(GPIO_AFRL_AFSEL7, 0b0101);
 
+    //reset SPI
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_SPE, 0b0);
 
-    SPI1->CR1 |= _VAL2FLD(SPI_CR1_BR, br);//set baud rate 
+    //SPI1->CR1 |= _VAL2FLD(SPI_CR1_BR, br);//set baud rate 
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_BR, 0b110);//set baud rate 
     SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPOL, cpol); //set clock polarity
     SPI1->CR1 |= _VAL2FLD(SPI_CR1_CPHA, cpha); //set clock phase
-    SPI1->CR2 |= _VAL2FLD(SPI_CR2_DS, 0b0111);//configure data size tp 8 bit
-    SPI1->CR1 |= _VAL2FLD(SPI_CR1_LSBFIRST, 0); //enable MSB first
-    SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSM, 1);//enable peripheral 
-    SPI1->CR1 |= _VAL2FLD(SPI_CR1_MSTR, 1);//enable controller 
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_LSBFIRST, 0b0); //enable MSB first
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSM, 0b0);//enable peripheral 
+    //SPI1->CR1 |= _VAL2FLD(SPI_CR1_SSI, 0b1);//enable peripheral control
+    SPI1->CR1 |= _VAL2FLD(SPI_CR1_MSTR, 0b1);//enable controller 
 
-    SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRXTH, 1); //set FIFO to 8bits
+    SPI1->CR2 |= _VAL2FLD(SPI_CR2_DS, 0b0111);//configure data size tp 8 bit
     SPI1->CR2 |= _VAL2FLD(SPI_CR2_SSOE, 1); //SS output is enabled
+    SPI1->CR2 |= _VAL2FLD(SPI_CR2_FRXTH, 1); //set FIFO to 8bits
     
     //enable SPI
     SPI1->CR1 |= _VAL2FLD(SPI_CR1_SPE, 0b1);
@@ -46,17 +51,17 @@ void initSPI(int br, int cpol, int cpha){
 char spiSendReceive(char send){
     while(!(SPI1->SR & SPI_SR_TXE)){}; // wait for empty transmit buffer
     
-    volatile char* address = (volatile char*)&SPI1->DR; // send data when it is written to a data register 
-    *address = send; 
+    // send data when it is written to a data register 
+    *(volatile char*)(&SPI1->DR) = send;
 
-    //while(!(SPI_SR_RXNE & SPI1->SR)){}; //wait for data to be recieved 
+    while(!(SPI_SR_RXNE & SPI1->SR)){}; //wait for data to be recieved 
 
-    return *address; //return the receieve data
+    return (SPI1->DR); //return the receieve data
 }
 
 float decodeData(int msb, char lsb){
     //get main data 
-    float temp = msb & 0b01111111; //mask data
+    float temp = msb & 0b01111111; //turn it into a float
     //get the sign bit 
     int sign = msb & (1 << 7); //8th bit
     if(!sign) { //when positive, add values
